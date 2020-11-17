a968ffe6e1c1569d0a204b0c4e779b1b1b11ffbc
jequezinho <jroverby92@gmail.com>
2020-11-17 16:11:56 -0600

    Managed to take data and turn into DataFrame!

--- team_data_table.py
+++ team_data_table.py
@@ -1,6 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas
 
 page = requests.get("https://www.baseball-reference.com/leagues/MLB/2019-standard-batting.shtml")
 soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.find('table'))
+my_table = soup.find('table')
+my_rows = [row for row in my_table.find_all('tr')]
+my_cells = [[float(cell.text) for cell in row.find_all('td')] for row in my_rows]
+my_cells = [cell for cell in my_cells if cell]
+print(pandas.DataFrame(my_cells))
