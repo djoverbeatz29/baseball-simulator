a6d1f60cb4fb897d2098004a2bf85c7ed9919976
jequezinho <jroverby92@gmail.com>
2020-11-18 11:22:32 -0600

    Got a fully functional DataFrame!

--- team_data_table.py
+++ team_data_table.py
@@ -5,7 +5,10 @@ import pandas
 page = requests.get("https://www.baseball-reference.com/leagues/MLB/2019-standard-batting.shtml")
 soup = BeautifulSoup(page.content, 'html.parser')
 my_table = soup.find('table')
+my_head = my_table.find('thead')
+my_head_cells = [cell.text for cell in my_head.find_all('th')]
 my_rows = [row for row in my_table.find_all('tr')]
 my_cells = [[float(cell.text) for cell in row.find_all('td')] for row in my_rows]
 my_cells = [cell for cell in my_cells if cell]
-print(pandas.DataFrame(my_cells))
+myDataFrame = pandas.DataFrame(data=my_cells, columns=my_head_cells[1:])
+print(myDataFrame)
